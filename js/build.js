1fade26ddb32a9632dbc7bdd86c0dcedb66b9a15
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-10-11 11:41:15 +0300

    update translation

--- js/build.js
+++ js/build.js
@@ -111,7 +111,7 @@
                 break;
               case 1:
                 // Summarise data
-                data.name = T('widgets.chart.pie.count') + ' ' + data.dataSourceQuery.columns.column;
+                data.name = T('widgets.chart.pie.count', { column: data.dataSourceQuery.columns.column });
                 result.dataSourceEntries.forEach(function(row) {
                   var value = row[data.dataSourceQuery.columns.column];
 
